38b920617dae86b874da3592627f51420e87c458
bwmelon97 <bwmelon97@naver.com>
2020-08-15 10:00:44 +0000

    Block Assignment Done

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
+import * as CryptoJS from 'crypto-js'
+
 class Block {
+    static calculateBlockHash = (
+        index: number,
+        previousHash: string,
+        data: string,
+        timeStamp: number
+    ): string => CryptoJS.SHA256(index + previousHash + data + timeStamp).toString();
+    
+    static validateStructure = (aBlock: Block): boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timeStamp === "number" &&
+        typeof aBlock.data === "string";
 
     public index: number;
     public hash: string;
@@ -21,9 +36,51 @@ class Block {
     }
 }
 
-const genesisBlock = new Block(1, '202020', '', 'Hello', 123456);
+const getBlockChain = (): Block[] => blockchain;
 
-let blockchain: [Block] = [genesisBlock];
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const genesisBlock = new Block(0, Block.calculateBlockHash(0, '', 'Hello', getNewTimeStamp()), '', 'Hello', getNewTimeStamp());
+let blockchain: Block[] = [genesisBlock];
+
+const createNewBlock = (data: string): Block => {
+    const prevBlock = getLatestBlock();
+    const newIndex = prevBlock.index + 1;
+    const newTimeStamp = getNewTimeStamp();
+    const newHash = Block.calculateBlockHash(
+        newIndex, prevBlock.hash, data, newTimeStamp
+    )
+    const newBlock = new Block(
+        newIndex, newHash, prevBlock.hash, data, newTimeStamp
+    )
+
+    addBlock(newBlock);
+
+    return newBlock;
+}
+
+const isBlockValid = (candidateBlock: Block, prevBlock: Block): boolean => 
+    Block.validateStructure(candidateBlock) &&
+    candidateBlock.index === prevBlock.index + 1 &&
+    candidateBlock.previousHash === prevBlock.hash &&
+    candidateBlock.hash === Block.calculateBlockHash(
+        candidateBlock.index,
+        candidateBlock.previousHash,
+        candidateBlock.data,
+        candidateBlock.timeStamp
+    ) 
+
+const addBlock = (aBlock: Block): void => {
+    const prevBlock = getLatestBlock();
+    if ( isBlockValid(aBlock, prevBlock) ) {
+        blockchain.push(aBlock);
+    }
+}
+
+createNewBlock('Second Block');
+createNewBlock('Bye Bye');
 
 console.log(blockchain);
 
