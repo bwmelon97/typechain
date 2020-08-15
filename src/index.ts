6dfa5e7d4d314a929d9c4c0b86c887f46758c14e
bwmelon97 <bwmelon97@naver.com>
2020-08-15 06:00:48 +0000

    learn class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-interface Person {
-    name: string;
-    age: number;
-    gender: string;
+// interface Person {
+//     name: string;
+//     age: number;
+//     gender: string;
+// }
+
+class Person {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor (name: string, age: number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const Soo = {
-    name: 'Soogeun',
-    age: 24,
-    gender: "male",
-}
+const Soo = new Person('Soogeun', 24, 'male');
 
 const sayHello = (person: Person): string => {
     return `hello, my name is ${person.name}, I'm ${person.age} years old. And I'm ${person.gender}.`;
