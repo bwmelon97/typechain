a72f8ec986a79fecb7590441b9c74bd511584336
bwmelon97 <bwmelon97@naver.com>
2020-08-15 05:13:14 +0000

    learn args

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var name = 'soogeun', age = '24', gender = 'male';
+var sayHello = function (name, age, gender) {
+    console.log("hello, my name is " + name + ", I'm " + age + " years old. And I'm " + gender + ".");
+};
+sayHello(name, age);
+//# sourceMappingURL=index.js.map
